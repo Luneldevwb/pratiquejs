b4ebfdeb398b4298bdf5d28f86a64af7cde762f0
Lunel <nematt007@gmail.com>
2023-03-15 09:22:47 +0300

    first commit

---
+++ src/js/scipt.js
@@ -0,0 +1,23 @@
+let money = prompt("Ваш бюджет на месяц?", ""),
+    time = prompt("Введите дату в формате YYYY-MM-DD");
+    
+let appData = {
+    budget : money,
+    timeData : time,
+    expenses : {"ответ на первый вопрос" : "ответ на второй вопрос"
+    },
+    optionalExpenses : {},
+    income : [],
+    savings : false 
+
+};
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", "")
+    a2 = prompt("Во сколько обойдется?", ""),
+    a3 = prompt("Введите обязательную статью расходов в этом месяце", "")
+    a4 = prompt("Во сколько обойдется?", "");
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert(appData.budget/30);
