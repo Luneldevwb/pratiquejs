3ccb860dcc5c59c975032ef8b9f25cb166bd4bf5
Lunel <nematt007@gmail.com>
2023-03-18 19:44:29 +0300

    lesson2 practice with our program and was used for do and while

--- src/js/scipt.js
+++ src/js/scipt.js
@@ -1,26 +1,79 @@
-// let money = prompt("Ваш бюджет на месяц?", ""),
-//     time = prompt("Введите дату в формате YYYY-MM-DD");
+let money = +prompt("Ваш бюджет на месяц?", ""),
+    time = prompt("Введите дату в формате YYYY-MM-DD");
     
-// let appData = {
-//     budget : money,
-//     timeData : time,
-//     expenses : {"ответ на первый вопрос" : "ответ на второй вопрос"
-//     },
-//     optionalExpenses : {},
-//     income : [],
-//     savings : false 
+let appData = {
+    budget : money,
+    timeData : time,
+    expenses : {
+        "ответ на первый вопрос" : "ответ на второй вопрос"
+    },
+    optionalExpenses : {},
+    income : [],
+    savings : false 
+
+};
+
+//added cycle 
+// for(let i = 0; i < 2; i++) {
+//     let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+//         b = prompt("Во сколько обойдется?", "");
+//     if( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50 ) {
+//         console.log('Done!');
+//         appData.expenses[a] = b;
+//     } else {
+//         console.log("bad result");
+//         i--;
+//     }
+// };
+
+// let i = 0;
+// while (i < 2) {
+//     let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+//         b = prompt("Во сколько обойдется?", "");
+//     if( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50 ) {
+//         console.log('Done!');
+//         appData.expenses[a] = b;
+//     } else {
+//         console.log("bad result");
+//         i--;
+        
+//     }
+//     i++;
 
 // };
 
-// let a1 = prompt("Введите обязательную статью расходов в этом месяце", "")
-//     a2 = prompt("Во сколько обойдется?", ""),
-//     a3 = prompt("Введите обязательную статью расходов в этом месяце", "")
-//     a4 = prompt("Во сколько обойдется?", "");
+let i = 0;
+do {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+        b = prompt("Во сколько обойдется?", "");
+    if( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50 ) {
+        console.log('Done!');
+        appData.expenses[a] = b;
+    } else {
+        console.log("bad result");
+        i--;
+        
+    }
+    i++;
+}
+while (i < 1);
 
-// appData.expenses.a1 = a2;
-// appData.expenses.a3 = a4;
 
-// alert(appData.budget / 30);
+appData.moneyPerDay = appData.budget / 30;
+
+
+alert("Бюджет на 1 день составляет: " + appData.moneyPerDay  + "руб.");
+
+if(appData.moneyPerDay < 100) {
+    console.log("Минимальный уровень достатка"); 
+} else if(appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний уровень достатка");
+} else if(appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+} else {
+    console.log("Произашла ошибка");
+}
+
 
 // if (2 * 4 == 8) {
 //     console.log('Vrai');
@@ -62,21 +115,24 @@
 
 //Cyles
 
-let num = 50;
-// while (num < 55) {
+// let num = 50;
+// // while (num < 55) {
+// //     console.log(num);
+// //     num++;
+// // }
+
+// do {
 //     console.log(num);
 //     num++;
 // }
+// while(num < 56);
 
-do {
-    console.log(num);
-    num++;
-}
-while(num < 56);
-
-for(let i = 1; i < 8; i++) {
-    if(i == 6) {
-        break;
-    }
-    console.log(i);
-}
+// for(let i = 1; i < 8; i++) {
+//     // if(i == 4) {
+//     //     break;
+//     // }
+//     if(i == 4) {
+//         continue;
+//     }
+//     console.log(i);
+// }
