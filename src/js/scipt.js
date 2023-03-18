e06bb0ee131c87d64734a5b51052bf981c055bb8
Lunel <nematt007@gmail.com>
2023-03-18 09:30:33 +0300

    new mod cycles

--- src/js/scipt.js
+++ src/js/scipt.js
@@ -63,7 +63,20 @@
 //Cyles
 
 let num = 50;
-while (num < 55) {
+// while (num < 55) {
+//     console.log(num);
+//     num++;
+// }
+
+do {
     console.log(num);
     num++;
+}
+while(num < 56);
+
+for(let i = 1; i < 8; i++) {
+    if(i == 6) {
+        break;
+    }
+    console.log(i);
 }
