547adaae68cdc7b7800e8e16a1af32f9d170ec43
Lunel <nematt007@gmail.com>
2023-03-20 18:12:27 +0300

    methodes et proprites for functions and another way to write function

--- lesson3/js.js
+++ lesson3/js.js
@@ -5,30 +5,36 @@
 //     let num = 10;
 // console.log(num);
 
-// }
+// }   //замыкание
 // showFirstMessage("Hello world");
-// console.log(num);
+// console.log(num);  
 
 //function declaration
-function calc(a, b) {
-    return(a + b);
-} 
-console.log(calc(2,4));
-console.log(calc(8,4));  //function declaration,les demandes peuv etre faites avant ou apres la funnction mere.
+// function calc(a, b) {
+//     return(a + b);
+// } 
 
+// console.log(calc(2,4));
+// console.log(calc(8,4));  //function declaration,les demandes peuv etre faites avant ou apres la funnction mere.
+
+//Function le standart EMAC6
+let calc = (a,b) => a + b
+console.log(calc(2,4));
+console.log(calc(8,4));
+//on peut ou ne pas mettre les accolades si c4est une petite function.
 //function expression
 
-let calc = function (a, b) {
-    return(a + b);
-} 
-console.log(calc(2,4)); //les demandes se font apres la funnction mere.
-console.log(calc(8,4)); //les demandes se font apres la funnction mere.
+// let calc = function (a, b) {
+//     return(a + b);
+// } 
+// console.log(calc(2,4)); //les demandes se font apres la funnction mere.
+// console.log(calc(8,4)); //les demandes se font apres la funnction mere.
 
 
-function retVar() {
-    let num = 50;
-    return num;
-}
+// function retVar() {
+//     let num = 50;
+//     return num;
+// }
 
-let anotherNum = retVar();
-console.log(anotherNum);
+// let anotherNum = retVar();
+// console.log(anotherNum);
