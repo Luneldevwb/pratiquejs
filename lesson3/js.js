1e32ac6ab0d5d4c470de8c8b70a1f5a3e9411adc
Lunel <nematt007@gmail.com>
2023-03-20 09:07:27 +0300

    types of function

--- lesson3/js.js
+++ lesson3/js.js
@@ -1,1 +1,34 @@
-console.log('Hello!');
+//Function
+// let num = 20;
+// function showFirstMessage(text) {
+//     alert(text);
+//     let num = 10;
+// console.log(num);
+
+// }
+// showFirstMessage("Hello world");
+// console.log(num);
+
+//function declaration
+function calc(a, b) {
+    return(a + b);
+} 
+console.log(calc(2,4));
+console.log(calc(8,4));  //function declaration,les demandes peuv etre faites avant ou apres la funnction mere.
+
+//function expression
+
+let calc = function (a, b) {
+    return(a + b);
+} 
+console.log(calc(2,4)); //les demandes se font apres la funnction mere.
+console.log(calc(8,4)); //les demandes se font apres la funnction mere.
+
+
+function retVar() {
+    let num = 50;
+    return num;
+}
+
+let anotherNum = retVar();
+console.log(anotherNum);
