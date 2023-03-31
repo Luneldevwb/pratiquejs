308a836dfe355ee79fddcfc368a12656a990fb13
Lunel <nematt007@gmail.com>
2023-03-31 16:53:06 +0300

    Methodes pour obtenir les elements

--- lesson4/l4.js
+++ lesson4/l4.js
@@ -5,6 +5,22 @@
 //-to string(transformer un elemnt en string)
 //Methode 1
 
-console.log(typeof(String(null)));
+// console.log(typeof(String(null)));
 
-alert( null || 2 && 3 || 4 );
+
+
+//Получение элементов со страницы(Methodes pour obtenir les elements)
+let box = document.getElementById('box'),
+    btn = document.getElementsByTagName('button'),
+    circle = document.getElementsByClassName('circle'),
+    heart = document.querySelectorAll('.heart');// c'est la methode la plus populaire.Elle est ensemble la methode forEach pour ranger les elememts. On peut utiliser un element parent suivi de son fils,il n y aura pas d'erreur.
+    oneHeart = document.querySelector('.heart')
+console.log(box);
+console.log(btn);
+console.log(btn[0]);
+console.log(circle[2]);
+console.log(heart[1]);
+
+
+
+
