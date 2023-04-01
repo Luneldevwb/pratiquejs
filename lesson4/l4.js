9ad0ba68a1f6aca09ce766a8b2f6f1c97c97fe3e
Lunel <nematt007@gmail.com>
2023-04-01 10:00:29 +0300

    lesson 4 methodes d'insertion d'elts dans le html a travers js

--- lesson4/l4.js
+++ lesson4/l4.js
@@ -13,13 +13,59 @@
 let box = document.getElementById('box'),
     btn = document.getElementsByTagName('button'),
     circle = document.getElementsByClassName('circle'),
-    heart = document.querySelectorAll('.heart');// c'est la methode la plus populaire.Elle est ensemble la methode forEach pour ranger les elememts. On peut utiliser un element parent suivi de son fils,il n y aura pas d'erreur.
-    oneHeart = document.querySelector('.heart')
-console.log(box);
-console.log(btn);
-console.log(btn[0]);
-console.log(circle[2]);
-console.log(heart[1]);
+    heart = document.querySelectorAll('.heart'),// c'est la methode la plus populaire.Elle est ensemble la methode forEach pour ranger les elememts. On peut utiliser un element parent suivi de son fils,il n y aura pas d'erreur.
+    wrapper = document.querySelector('.wrapper'),
+    oneHeart = document.querySelector('.heart');
+
+// console.log(box);
+// console.log(btn);
+// console.log(btn[0]);
+// console.log(circle[2]);
+// console.log(heart[1]);
+// console.log(oneHeart);
+
+//APPRENDRE A STYLISER LES ELEMENTS DEPUIS Js
+box.style.backgroundColor = 'blue';
+box.style.width = '150px';
+box.style.height = '150px';
+box.style.padding = '20px';
+box.style.marginBottom = '20px';
+circle[0].style.backgroundColor = 'green';
+circle[1].style.backgroundColor = 'yellow';
+circle[2].style.backgroundColor = 'red';
+
+
+
+
+
+//utilser un cylce pour attenidre tout les elements du selecteur.
+for( let i = 0; i < btn.length; i++ ) {
+    btn[i].style.width = '75px';
+    btn[i].style.height = '40px';
+    btn[i].style.backgroundColor = '#b7dba6';
+
+}
+//Ou la methode forEach
+// heart.forEach(function(items, i, hearts) {
+//     items.style.backgroundColor = 'orange';
+// });
+
+//Creation d'element depuis JS
+let div = document.createElement('div'),
+    text = document.createTextNode('I was here!'); //Creation d'1 texte sans tag.
+div.classList.add('black');
+// document.body.appendChild(div); // pour ajouter un elemnt au bas de la lignee parentale.
+// wrapper.appendChild(div);
+
+div.style.width = '100px';
+div.style.height = '100px';
+div.style.backgroundColor = '#54cbcb';
+div.style.marginTop = '20px';
+
+document.body.insertBefore(div, circle[0]); //ajouter un element devant un autre.
+
+
+
 
 
 
