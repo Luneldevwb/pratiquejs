91db7a43e52bdf2806b0952bad327c3d168688b6
Lunel <nematt007@gmail.com>
2023-04-05 09:25:50 +0300

    creation des variables pour budgetapp

---
+++ budget app/src/js/main.js
@@ -0,0 +1,8 @@
+let btn = document.getElementById('start'),
+    daybudgetValue = document.getElementsByClassName('daybudget-value'),
+    levelValue = document.getElementsByClassName('level-value'),
+    expensesValue = document.getElementsByClassName('expenses-value'),
+    incomeValue = document.getElementsByClassName('income-value'),
+    monthSavingsValue = document.getElementsByClassName('monthsavings-value'),
+    yearSavingsValue = document.getElementsByClassName('yearsavings-value');
+
