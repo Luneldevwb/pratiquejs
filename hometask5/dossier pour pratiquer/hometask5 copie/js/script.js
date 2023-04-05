d54b359b167160ac4691ee420d80772b2094cb5c
Lunel <nematt007@gmail.com>
2023-04-05 09:08:10 +0300

    pratique des elements du cours 4

--- hometask5/dossier pour pratiquer/hometask5 copie/js/script.js
+++ hometask5/dossier pour pratiquer/hometask5 copie/js/script.js
@@ -2,13 +2,18 @@ let menu = document.getElementsByClassName('menu')[0],
     menuItem = document.getElementsByClassName('menu-item'),
     title = document.getElementById('title'),
     adv = document.getElementsByClassName('adv')[0],
-    prompt = document.querySelector('#prompt'),
+    promptforApple = document.querySelector('#prompt'),
     menuItemLi = document.createElement('li');
 menu.insertBefore(menuItem[2], menuItem[1]); //on commence d'abord par le nouveau element  puis l'ancien.
-
 
 menuItemLi.classList.add('menu-item');
 menuItemLi.textContent = 'Пятый пункт';
 menu.appendChild(menuItemLi);
 
-document.body.style.backgroundImage = "url('../img/apple_true.jpg')";
+
+document.body.style.backgroundImage = "url('img/apple_true.jpg')";
+
+title.textContent = 'Мы продаем только подлинную технику Apple';
+adv.remove();
+let yourOpinion = prompt('Как вы думаете насчет наших продуктов?', '');
+promptforApple.textContent = yourOpinion;
