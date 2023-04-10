288c02df12862f9b100492b081b31133ac642e65
Lunel <nematt007@gmail.com>
2023-04-10 10:39:38 +0300

    new mod

--- lesson6/l6.js
+++ lesson6/l6.js
@@ -1,7 +1,7 @@
 //События и их обработчики
 //onclick="alert('You click on 1 button')" ce n'est pas recommende d'utiliser directement le script dans html
 
-let btn = document.getElementsByTagName('button'),
+let btn = document.querySelector('button'),
     wrap = document.querySelector('.wrapper'),
     link = document.querySelector('a'); 
 
@@ -13,15 +13,27 @@ let btn = document.getElementsByTagName('button'),
 // }; 
 //la 1ere fonction sera annulee,au profit de la nouvelle,pour eviter cela on utlise addeEventListner methode
 //Evenemment click
-btn[0].addEventListener('click', function(event) {
-    // console.log(event);//pour savoir quels elements contient l'object.
-    // let target = event.target;
-    // target.style.display = 'none';// on peut utiliser target separement.
-    console.log('Event was done: ' + event.type + ' on element ' + event.target);
-});//pour savoir quel type d'evenements a ete realise.
+// btn[0].addEventListener('click', function(event) {
+//     // console.log(event);//pour savoir quels elements contient l'object.
+//     // let target = event.target;
+//     // target.style.display = 'none';// on peut utiliser target separement.
+//     console.log('Event was done: ' + event.type + ' on element ' + event.target);
+// });//pour savoir quel type d'evenements a ete realise.
 
-wrap.addEventListener('click', function() {
+// wrap.addEventListener('click', function() {
+//     console.log('Event was done: ' + event.type + ' on element ' + event.target);
+// });
+
+link.addEventListener('click', function(event) {
+    event.preventDefault();//pour annuler les parametres par defauts des navigateurs
     console.log('Event was done: ' + event.type + ' on element ' + event.target);
+});
+
+//pour utiliser tout les elements aussitot
+btn.forEach( function(item) {
+    item.addEventListener('mouseleave', function() {
+        console.log('Log out!');
+    });
 });
 
 // btn[0].addEventListener('click', function() {
