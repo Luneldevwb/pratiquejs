91e4422aa31b5eaf04e3078274002d1afa61baa7
Lunel <nematt007@gmail.com>
2023-04-18 08:58:45 +0300

    debut de la formation de notre script.

---
+++ yoga/js/script.js
@@ -0,0 +1,6 @@
+// On va utiliser Domcontentloaded par rapport a loaded simple,car il permet de tout charger et lancer le script meme tout les docs ne sont pas encore pret.
+
+window.addEventListener('DOMContentLoaded', function() {
+    // Ici qu'on ecrira le script de notre page.
+    'use strict';
+});
