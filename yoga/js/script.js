75d888509b21ac886c896402215bc874552304ea
Lunel <nematt007@gmail.com>
2023-04-18 17:24:19 +0300

    J'ai fait le script pour animer les tabs.

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -3,4 +3,39 @@
 window.addEventListener('DOMContentLoaded', function() {
     // Ici qu'on ecrira le script de notre page.
     'use strict';
+
+    let tab = document.querySelectorAll('.info-header-tab'),
+        info = document.querySelector('.info-header'), // si tu utilises getelemtbytagname il faut tjrs l'indice a la fin.
+        tabContent = document.querySelectorAll('.info-tabcontent');
+    
+    function hideTabContent(a) {
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+
+
+        }
+    }
+    hideTabContent(1);
+
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    } 
+
+    info.addEventListener('click', function(event) {
+
+        let target = event.target;
+        if (target && target.classList.contains('info-header-tab')) {
+            for (let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+    });
 });
