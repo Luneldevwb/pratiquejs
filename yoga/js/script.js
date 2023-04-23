71a420b54a1be35597799a0e1ad628e1fcaf019e
Lunel <nematt007@gmail.com>
2023-04-23 09:44:02 +0300

    J'ai mis le timer en marche.

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -1,6 +1,6 @@
 // On va utiliser Domcontentloaded par rapport a loaded simple,car il permet de tout charger et lancer le script meme tout les docs ne sont pas encore pret.
 
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', function () {
     // Ici qu'on ecrira le script de notre page.
     'use strict';
     // L'activation des tabs dans une page.
@@ -8,7 +8,7 @@ window.addEventListener('DOMContentLoaded', function() {
     let tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'), // si tu utilises getelemtbytagname il faut tjrs l'indice a la fin.
         tabContent = document.querySelectorAll('.info-tabcontent');
-    
+
 
     // 2e etape on cree ue fonction ayant un cycle permetant de cacher le contenu des tabs
     function hideTabContent(a) {
@@ -27,10 +27,10 @@ window.addEventListener('DOMContentLoaded', function() {
             tabContent[b].classList.remove('hide');
             tabContent[b].classList.add('show');
         }
-    } 
+    }
 
     // 4e etape on cree ue fonction ayant un cycle de cacher et d'afficher le contenu des tabs tout en precisant le tab selectionner avec les paarametres target et event.
-    info.addEventListener('click', function(event) {
+    info.addEventListener('click', function (event) {
 
         let target = event.target;
         if (target && target.classList.contains('info-header-tab')) {
@@ -44,21 +44,65 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-// Создаем таймер обратного отсчета на сайте
+    // Создаем таймер обратного отсчета на сайте
 
-// 1ere etape on cree une date limite    
-let deadline = '2023-10-21';
+    // 1ere etape on cree une date limite    
+    let deadline = '2023-05-05';
 
-// 2 etape on cree une qui nous permet d'obtenir et l'envoyer au span correspondant.
+    // 2 etape on cree une qui nous permet d'obtenir et l'envoyer au span correspondant.
 
-function getTimeRemaining(endtime) {
-    let t = Date.parse(endtime) - Date.parse(new Date()),
-        seconds = Math.floor((t/1000) % 60),
-        minutes =  Math.floor((t/1000/60) % 60),
-        hours =  Math.floor((t/(1000*60*60)));
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t / 1000) % 60),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            hours = Math.floor((t / (1000 * 60 * 60)));
         //hours =  Math.floor((t/1000/60/60) % 24) //les heures
-       // days = Math.floor((t/(1000*60*60*24))) //pour les jours
-}
+        // days = Math.floor((t/(1000*60*60*24))) //pour les jours
+
+        // on cree un object pour obtenir un resultat apres l'execution de la fonction.
+        return {
+            'total': t,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+
+
+        };
+    }
+
+    // 3 etape on cree une fonction pour rendre notre timer dynamique en creant une autre fonction.
+
+    function setClock(id, endtime ) {   // ca prend deux areuments: 1.la boite ou se trouve notre timer et 2. l'argument qui est lie a la fonction compteur.
+
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'), // on prend la classe hours selon son parent direct qui est timer.
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+        
+
+        // 4 etape il faut creer une function qui mettra a jour notre timer chaque seconde en utilisant la methode setInterval.
+
+        function updateClock() {
+            // on cree un parametre de controle t
+            let t = getTimeRemaining(endtime); // deadline vient en tant que argument dans endtime.
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes; // pour mettre a jour notre  timer(hours,minutes,seconds) on doit les obtenir de l'object t,car c'est labas que se trouve la maj de chaque h, m et s.
+
+            seconds.textContent = t.seconds;
+
+            // 5 etape il nous faut creer une condition pour arreter le timer une fois le temps epuiser.
+            
+            if(t.total < 0) {
+                clearInterval(timeInterval);
+            }
+
+
+        }
+
+    }       
+    
+
+    setClock('timer', deadline);
 
 });
-
