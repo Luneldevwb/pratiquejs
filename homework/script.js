b51cc1e84993378d1d741bded533d0447fab5175
Lunel <nematt007@gmail.com>
2023-05-21 14:41:45 +0300

    Standart ES6 1er point.
    Interpolation ES6

--- homework/script.js
+++ homework/script.js
@@ -1,7 +1,15 @@
 
 // Выведите на экран правильное сообщение, которое берет значение из input
-let age = document.getElementById('age');
-function showUser(surname, name) {
-alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
-}
-console.log(showUser.apply(age, ['Nzaou','Lunel']));
+// let age = document.getElementById('age');
+// function showUser(surname, name) {
+// alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+// }
+// console.log(showUser.apply(age, ['Nzaou','Lunel']));
+
+// Стандарт ES6. Интерполяция
+
+let name = 'Lune',
+    age = 30,
+    mail = 'email@getMaxListeners.com';
+
+document.write(`User ${name} ${age} years old. His adresse email: ${mail}`);
