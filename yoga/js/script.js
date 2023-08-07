babe3b8799f814ab729b66235da8ecd9f61c9865
Lunel <nematt007@gmail.com>
2023-08-08 00:25:25 +0300

    No update was made.

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -192,7 +192,7 @@ window.addEventListener('DOMContentLoaded', function () {
             }
         });
 
-        for(let i = 0; i < input.length; i++) {
+        for (let i = 0; i < input.length; i++) {
             input[i].value = '';
         }
 
