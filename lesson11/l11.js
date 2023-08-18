d13e39f078800290c53d7c431cf1983185e0417a
Lunel <nematt007@gmail.com>
2023-08-19 00:41:34 +0300

    rewrite inputdata with promise

--- lesson11/l11.js
+++ lesson11/l11.js
@@ -15,28 +15,91 @@
 let inputRub = document.getElementById('rub'),
     inputUsd = document.getElementById('usd');
 
+
 inputRub.addEventListener('input', () => {
-    let request = new XMLHttpRequest();
 
-    //Methods Ajax
-    //request.open(method(POST,GET), url, async, login,pass )
+    function catchData() {
+        return new Promise(function (resolve, reject) {
+            let request = new XMLHttpRequest();
 
-    request.open('GET', '../lesson11/current.json');
-    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-    request.send();
+            //Methods Ajax
+            //request.open(method(POST,GET), url, async, login,pass )
 
-    //Proprietes Ajax
-    //Status( ca peut 404 ou autre)
-    //StatusText(contient la reponse par expl ok ou not found)
-    // responseText (contient le texte et la reponse du serveur) / response
-    //readyState(l'etat actuel de la demande);
+            request.open('GET', '../lesson11/current.json');
+            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+            request.send();
 
-    request.addEventListener('readystatechange', function() {
-        if(request.readyState === 4 && request.status == 200) {
-            let data = JSON.parse(request.response);
+            //Proprietes Ajax
+            //Status( ca peut 404 ou autre)
+            //StatusText(contient la reponse par expl ok ou not found)
+            // responseText (contient le texte et la reponse du serveur) / response
+            //readyState(l'etat actuel de la demande);
 
-            inputUsd.value = inputRub.value / data.usd;
-        } else inputUsd.value = 'Something was wrong!';
-    });
+            request.onload = function () {
+                if(request.readyState === 4) {
+                    if(request.status == 200) {
+                        resolve(this.response);
+                    }
+                    else {
+                        reject();
+                    }
+                } 
+               
+            }
+        });
+    };
 
+    catchData()
+    .then(response => {
+        // console.log(response);
+        let data = JSON.parse(response);
+        inputUsd.value = inputRub.value / data.usd;
+    })
+    // .then(() => console.log(5000))
+    .catch(() => inputUsd.value = 'Something was wrong')
 });
+
+// inputRub.addEventListener('input', () => {
+//     let request = new Promise((resolve, reject) => {
+//         let xhr = new XMLHttpRequest();
+//         xhr.open('GET', '../lesson11/current.json');
+//         xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+//         xhr.onload = () => xhr.status == 200 ? resolve(xhr.response) : reject('Error: ' + xhr.status);
+//         xhr.onerror = () => reject('Network Error');
+//         xhr.send();
+//     });
+
+//     request.then(response => {
+//         let data = JSON.parse(response);
+//         inputUsd.value = inputRub.value / data.usd;
+//     }).catch(error => {
+//         inputUsd.value = 'Something was wrong!';
+//         console.error('There was an error!', error);
+//     });
+// });
+
+// inputRub.addEventListener('input', () => {
+//     let request = new XMLHttpRequest();
+
+//     //Methods Ajax
+//     //request.open(method(POST,GET), url, async, login,pass )
+
+//     request.open('GET', '../lesson11/current.json');
+//     request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+//     request.send();
+
+//     //Proprietes Ajax
+//     //Status( ca peut 404 ou autre)
+//     //StatusText(contient la reponse par expl ok ou not found)
+//     // responseText (contient le texte et la reponse du serveur) / response
+//     //readyState(l'etat actuel de la demande);
+
+//     request.addEventListener('readystatechange', function () {
+//         if (request.readyState === 4 && request.status == 200) {
+//             let data = JSON.parse(request.response);
+
+//             inputUsd.value = inputRub.value / data.usd;
+//         } else inputUsd.value = 'Something was wrong!';
+//     });
+
+// });
