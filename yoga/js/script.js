e8f4cd997745bf62cdf31180cb4658e47caf8c1c
Lunel <nematt007@gmail.com>
2023-08-19 00:40:22 +0300

    Rewrite senddata with promise

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -160,42 +160,110 @@ window.addEventListener('DOMContentLoaded', function () {
    
     // Реализация скрипта отправки данных из формы
     
+    // let message = {
+    //     loading: 'Загрузка',
+    //     success: 'Спасибо! Скоро свяжемся с вами!',
+    //     failure: 'Что-то пошло не так...'
+    // }
+
+    // let form = document.querySelector('.main-form'),
+    //     formBottom = document.getElementById('form'),
+    //     input = document.querySelector('input'),
+    //     statusMessage = document.createElement('div');
+    //     statusMessage.classList.add('status');
+    
+    // form.addEventListener('submit', function(event) {
+    //     event.preventDefault();
+    //     form.appendChild(statusMessage);
+        
+    //     let request = new XMLHttpRequest();
+    //     request.open('POST', 'server.php');
+    //     request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+    //     let formData = new FormData(form);
+    //     request.send(formData);
+
+    //     request.addEventListener('readystatechange', function() {
+    //         if (request.readystate < 4) {
+    //             statusMessage.innerHTML = message.loading;
+    //         } else if (request.readyState === 4) {
+                    //if (request.status == 200 && request.status < 300) {
+                          // thanksModal.style.display = 'block'; A AJOUTER
+                         //MainModal.style.display = 'none'; A AJOUTER
+    //                    statusMessage.innerHTML = message.success;
+                  //  }
+    //         } else {
+    //             statusMessage.innerHTML = message.failure;
+    //         }
+    //     });
+
+    //     for (let i = 0; i < input.length; i++) {
+    //         input[i].value = '';
+    //     }
+
+
+    // }); // On attribue a la forme et non au boutton/submit
+    // sendForm(form);
+    // sendform(formBottom);
+
     let message = {
         loading: 'Загрузка',
         success: 'Спасибо! Скоро свяжемся с вами!',
         failure: 'Что-то пошло не так...'
     }
-
+    
     let form = document.querySelector('.main-form'),
-        input = form.querySelector('input'),
+        formBottom = document.getElementById('form'),
+        input = document.querySelector('input'),
         statusMessage = document.createElement('div');
         statusMessage.classList.add('status');
     
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
-        form.appendChild(statusMessage);
+    
+    function sendForm(elem) {
+        elem.addEventListener('submit', function(event) {
+            event.preventDefault();
+            elem.appendChild(statusMessage);
         
-        let request = new XMLHttpRequest();
-        request.open('POST', 'server.php');
-        request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-
-        let formData = new FormData(form);
-        request.send(formData);
-
-        request.addEventListener('readystatechange', function() {
-            if (request.readystate < 4) {
-                statusMessage.innerHTML = message.loading;
-            } else if (request.readyState === 4 && request.status == 200) {
-                statusMessage.innerHTML = message.success;
-            } else {
-                statusMessage.innerHTML = message.failure;
+            let formData = new FormData(elem);
+        
+            function postData(data) {
+                return new Promise(function(resolve, reject) {
+                    let request = new XMLHttpRequest();
+                    request.open('POST', 'server.php');
+                    request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        
+                    request.onreadystatechange = function() {
+                        if (request.readyState < 4) {
+                            resolve();
+                        } else if (request.readyState === 4) { 
+                            if (request.status == 200 && request.status < 300) {
+                            resolve();
+                            }
+                        } else {
+                            reject();
+                        }
+                    }
+                    request.send(data);
+                });
+            } //end postData
+        
+            function clearInput() {
+                for (let i = 0; i < input.length; i++) {
+                    input[i].value = '';
+                }
             }
+        
+            postData(formData)
+                .then(() => statusMessage.innerHTML = message.loading)
+                .then(() => {
+                    statusMessage.innerHTML = message.success;
+                })
+                .catch(() => statusMessage.innerHTML = message.failure)
+                .then(clearInput)
         });
+    }
+    sendForm(form);
+    sendForm(formBottom);
 
-        for (let i = 0; i < input.length; i++) {
-            input[i].value = '';
-        }
-
-    }); // On attribue a la forme et non au boutton/submit
 
 });
